349fb86bd7030f8fdef4596ebf1554e292ab83a0
Azeez-Ismaila <azeezthegenius@gmail.com>
2022-08-15 17:03:53 +0100

    final phase

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
+window.addEventListener("load", () => {
+  document.querySelector(".main").classList.remove("hidden");
+  document.querySelector(".home-section").classList.add("active");
+  // -----------Page Loader------------
+  document.querySelector(".page-loader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".page-loader").style.display = "none";
+  }, 600);
+});
+// ------------ Toggle Navbar ------------
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavBar();
+  document.body.classList.toggle("hide-scrolling");
+});
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavBar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+
+// ------------------ Active Section ---------------------
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    // Activate Overlay to prevent multiple clicks
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavBar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+
 // ----------- About Tabs-------
 const tabsContainer = document.querySelector(".about-tabs"),
   aboutSection = document.querySelector(".about-section");
@@ -47,3 +95,70 @@ let portfolioItemDetails = (portfolioItem) => {
     ".portfolio-item-details"
   ).innerHTML;
 };
+
+// EMAIL JS
+
+// window.onload = function () {
+//   console.log("emailjs");
+//   document
+//     .getElementById("contact-form")
+//     .addEventListener("submit", function (event) {
+//       event.preventDefault();
+//       // generate a five digit number for the contact_number variable
+//       this.contact_number.value = (Math.random() * 100000) | 0;
+//       // these IDs from the previous steps
+//       emailjs.sendForm("contact_service", "contact_form", this).then(
+//         function () {
+//           console.log("SUCCESS!");
+//         },
+//         function (error) {
+//           console.log("FAILED...", error);
+//         }
+//       );
+//     });
+// };
+// FORM VALIDATION
+function formValidation(e) {
+  e.preventDefault();
+  let fullname = document.getElementById("fullname").value;
+  const email = document.getElementById("email_id").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  if (
+    fullname.length < 1 ||
+    email.length < 1 ||
+    subject.length < 1 ||
+    message.length < 1
+  ) {
+    return;
+  } else {
+    sendMail();
+    document.getElementById("fullname").value = "";
+    document.getElementById("email_id").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+  }
+}
+const submitForm = document.getElementById("submitContact");
+
+submitForm.addEventListener("click", formValidation);
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("aPX13V3nyg3Ia5CzL");
+})();
+function sendMail() {
+  console.log("btn clicked");
+
+  var inputs = {
+    from_name: document.getElementById("fullname").value,
+    email_id: document.getElementById("email_id").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  // alert(JSON.stringify(inputs));
+  emailjs.send("Azeez9744", "template_ofcp2mm", inputs).then(function (res) {
+    console.log(res);
+  });
+}
